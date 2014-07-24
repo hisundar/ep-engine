a32c9c87315821c4c6e7c54a6e526bc3a2d6b7ca
Sundar Sridharan <sundar.sridharan@gmail.com>
2014-07-24 15:52:28 -0700

    Add stats for measuring ExecutorPool scheduling delay & runtimes
    ./cbstats ip:port -b <bucketname> scheduler
    returns the timing histograms of the scheduling overhead incurred by
    all tasks in ep-engine
    Scheduling overhead is computed as the difference between the scheduled
    runtime of a task and the actual runtime
    This will give us definitive answers to how much delay is introduced by
    the scheduling overhead of the global thread pool and how it affects
    each task in ep-engine
    cbstats ip:port -b <bucketname> runtimes
    returns the timing histograms of various task runtimes measured from the
    ExecutorPool
    Change-Id: Icb34d4a87c8659fb22b7d2c1ba6bef0836045c3e

--- src/ep.cc
+++ src/ep.cc
@@ -192,6 +192,14 @@ EventuallyPersistentStore::EventuallyPersistentStore(
 
     storageProperties = new StorageProperties(true, true, true, true);
 
+    stats.schedulingHisto = new Histogram<hrtime_t>[MAX_TYPE_ID];
+    stats.taskRuntimeHisto = new Histogram<hrtime_t>[MAX_TYPE_ID];
+
+    for (type_id_t i = (type_id_t)0; i < MAX_TYPE_ID; i++) {
+        stats.schedulingHisto[i].reset();
+        stats.taskRuntimeHisto[i].reset();
+    }
+
     ExecutorPool::get()->registerBucket(ObjectRegistry::getCurrentEngine());
 
     vb_mutexes = new Mutex[config.getMaxVbuckets()];
@@ -363,6 +371,8 @@ EventuallyPersistentStore::~EventuallyPersistentStore() {
     ExecutorPool::get()->unregisterBucket(ObjectRegistry::getCurrentEngine());
 
     delete [] vb_mutexes;
+    delete [] stats.schedulingHisto;
+    delete [] stats.taskRuntimeHisto;
     delete conflictResolver;
     delete warmupTask;
     delete storageProperties;
@@ -2997,6 +3007,11 @@ void EventuallyPersistentStore::resetUnderlyingStats(void)
         KVShard *shard = vbMap.shards[i];
         shard->getRWUnderlying()->resetStats();
         shard->getROUnderlying()->resetStats();
+    }
+
+    for (type_id_t i = (type_id_t)0; i < MAX_TYPE_ID; i++) {
+        stats.schedulingHisto[i].reset();
+        stats.taskRuntimeHisto[i].reset();
     }
 }
 
